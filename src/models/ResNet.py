202d664cb7f32ccc765931f0bcd01580a28036ff
Joao Felipe <joao.rocha@student-cs.fr>
2022-01-13 16:28:24 +0100

    [+] Resnet implementation to be tested in cluster

--- src/models/ResNet.py
+++ src/models/ResNet.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 
 
 class ResidualBlock(nn.Module):  # pylint: disable=too-few-public-methods
-    """Define our Residual Block"""
+    """Define our Residual model Block"""
 
     def __init__(
         self, in_channels, out_channels, stride=1, kernel_size=3, padding=1, bias=False
@@ -16,7 +16,7 @@ class ResidualBlock(nn.Module):  # pylint: disable=too-few-public-methods
                 in_channels, out_channels, kernel_size, stride, padding, bias=bias
             ),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(True),
+            nn.ReLU(inplace=True),
         )
         self.cnn2 = nn.Sequential(
             nn.Conv2d(out_channels, out_channels, kernel_size, 1, padding, bias=bias),
@@ -45,22 +45,20 @@ class ResidualBlock(nn.Module):  # pylint: disable=too-few-public-methods
         x = self.cnn1(x)
         x = self.cnn2(x)
         x += self.shortcut(residual)
-        x = nn.ReLU(True)(x)
+        x = nn.ReLU(inplace=True)(x)
         return x
 
 
-class ResNet(
-    nn.Module
-):  # pylint: disable=too-few-public-methods disable=too-many-instance-attributes
-    """Define our Residual Network model"""
+class ResNet(nn.Module):  # pylint: disable=too-few-public-methods
+    """Define our Residual model Network"""
 
-    def __init__(self, input_size, num_classes):
+    def __init__(self, input_size, num_classes):  # pylint: disable=unused-argument
         super(ResNet, self).__init__()  # pylint: disable=super-with-arguments
 
         self.block1 = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=2, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
-            nn.ReLU(True),
+            nn.ReLU(inplace=True),
         )
 
         self.block2 = nn.Sequential(
@@ -73,18 +71,10 @@ class ResNet(
         self.block5 = nn.Sequential(ResidualBlock(256, 512), ResidualBlock(512, 512, 2))
 
         self.avgpool = nn.AvgPool2d(2)
-        # vowel_diacritic
-        self.fc1 = nn.Linear(512, 11)
-        # grapheme_root
-        self.fc2 = nn.Linear(512, 168)
-        # consonant_diacritic
-        self.fc3 = nn.Linear(512, 7)
 
-        self.classifier = nn.Linear(
-            (input_size // 4) * (input_size // 4) * 32, num_classes
-        )
+        self.classifier = nn.Linear(12800, num_classes)
 
-    def forward(self, x):  # pylint : disable=invalid-name, redefined-outer-name
+    def forward(self, x):
         """Define the forward method
 
         Args:
@@ -93,22 +83,36 @@ class ResNet(
         Returns:
             torch.Tensor: labels
         """
+        print("I started my adventure as:")
+        print(x.shape)
         x = self.block1(x)
+        print("I survived block 1 and left as")
+        print(x.shape)
         x = self.block2(x)
+        print("I survived block 2 and left as")
+        print(x.shape)
         x = self.block3(x)
+        print("I survived block 3 and left as")
+        print(x.shape)
         x = self.block4(x)
+        print("I survived block 4 and left as")
+        print(x.shape)
         x = self.block5(x)
+        print("I survived block 5 and left as")
+        print(x.shape)
         x = self.avgpool(x)
+        print("I survived the avg pooling and left as")
+        print(x.shape)
         x = x.view(x.size(0), -1)
-        x1 = self.fc1(x)
-        x2 = self.fc2(x)
-        x3 = self.fc3(x)
-        return x1, x2, x3
+        print("I changed using the view to:")
+        print(x.shape)
+        x = self.classifier(x)
+        return x
 
 
 if __name__ == "__main__":
     x = torch.randn(2, 1, 300, 300)
-    model = ResNet(300, 10)
+    model = ResNet(input_size=300, num_classes=10)
     output = model(x)
     # Should return torch.Size([2, 10])
     print(output.shape)
